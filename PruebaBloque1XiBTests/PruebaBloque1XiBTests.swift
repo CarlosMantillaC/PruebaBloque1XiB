46d158cdc1811e8a1664bf93875a7271ac248345
Carlos Mantilla <carlosmcot@gmail.com>
2025-06-05 15:47:36 -0500

    crea proyecto base

---
+++ PruebaBloque1XiBTests/PruebaBloque1XiBTests.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaBloque1XiBTests.swift
+//  PruebaBloque1XiBTests
+//
+//  Created by Gopenux on 5/06/25.
+//
+
+import Testing
+@testable import PruebaBloque1XiB
+
+struct PruebaBloque1XiBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
