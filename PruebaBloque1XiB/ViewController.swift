a3a951e8d6d1cd8920abded332df51b5190c2a29
Carlos Mantilla <carlosmcot@gmail.com>
2025-06-05 16:26:23 -0500

    agrega diseño e implementacion del login

--- PruebaBloque1XiB/ViewController.swift
+++ PruebaBloque1XiB/ViewController.swift
@@ -7,23 +7,37 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    
+    let usuarioValido = "Admin"
+    let contrasenaValida = "1234"
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var userTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func login(_ sender: Any) {
+        
+        let usuario = userTextField.text ?? ""
+        let contrasena = passwordTextField.text ?? ""
+        
+        if usuario == usuarioValido && contrasena == contrasenaValida {
+            
+            self.navigationController?.pushViewController(ViewControllerReproductor(nibName: "ViewControllerReproductor", bundle: nil), animated: true)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        } else {
+            let alerta = UIAlertController(title: "Error", message: "Credenciales Incorrectas", preferredStyle: .alert)
+            alerta.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alerta, animated: true)
+        }
     }
-    */
-
+    
+    
 }
